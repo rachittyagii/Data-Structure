0089f83f8fb33bc591889885ea12040c5ad18009
Rachit Tyagi <rachittyagi@duck.com>
2023-08-25 12:24:08 +0530

    second

--- 2 Excercise.py
+++ 2 Excercise.py
@@ -1,18 +1,21 @@
 import numpy as np
-def sort(a,n):
-   for i in range(1,n):
-    key=a[i]
-    j=i-1
-    while(a[j]>key and j>=0):
-       a[j+1]=a[j]
-       j=j-1
-       a[j+1]=a[j]
-       return a
 
-n=int(input("Enter no of elements"))
-np.array([None]*n)
+def sort(a, n):
+    for i in range(1, n):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > key:
+            a[j + 1] = a[j]
+            j = j - 1
+        a[j + 1] = key
+    return a
+
+n = int(input("Enter no of elements: "))
+a = np.array([None] * n)  # Corrected array initialization
+
 for i in range(n):
-   a[i]=int(input())
+    a[i] = int(input())
+
 print(a)
-b=sort(a,n)
+b = sort(a, n)
 print(b)
